52641b9c45a956bcc8b447eebb1a5755b24b3d2a
Chad <2420811595@qq.com>
2024-09-16 22:12:57 +0800

    feature:添加mybatis-plus对BaseDO字段的自动填充配置

---
+++ src/main/java/org/chad/bytecinema/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package org.chad.bytecinema.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Primary
+@Component(value = "myMetaObjectHandlerByAdmin")
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+        strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        strictInsertFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
